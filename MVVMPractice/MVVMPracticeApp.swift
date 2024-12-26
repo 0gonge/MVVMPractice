a422b4204152b22a5d73f8e3c07b370616faa69b
0gonge <Challengef0802@gmail.com>
2024-12-27 02:43:07 +0900

    docs: gitignore 생성

---
+++ MVVMPractice/MVVMPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMPracticeApp.swift
+//  MVVMPractice
+//
+//  Created by 송여경 on 12/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVMPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
